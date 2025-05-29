985524748c23e1acaa18e741a58fe12055e3487b
Manisha-athili <manishaathili1126@gmail.com>
2025-05-29 12:47:57 +0530

    frontend axios added

--- FrontEnd/src/Features/Prompts/PromptService.js
+++ FrontEnd/src/Features/Prompts/PromptService.js
@@ -1,13 +1,6 @@
-import axios from 'axios';
+import api from '../../api/axios';
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api/prompts';
 
-export const fetchPrompts = async () => {
-  const res = await axios.get(API_BASE);
-  return res.data;
-};
-
-export const savePrompt = async (prompt) => {
-  const res = await axios.post(API_BASE, { prompt });
-  return res.data;
-};
+export const searchPrompts = (query) => api.get(`/prompts/search?searchWord=${query}`);
+export const getAllPrompts = () => api.get('/prompts');
+export const getPromptById = (id) => api.get 
